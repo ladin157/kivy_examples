410163e9be7dc7e6b9711dba92ec05ed4ab3f11e
Noi Nguyen <noi.nguyen@lqdtu.edu.vn>
2020-04-05 17:36:17 +0700

    Test cv2

--- kivy_cv2/kivy_cv.py
+++ kivy_cv2/kivy_cv.py
@@ -4,12 +4,13 @@ from kivy.clock import Clock
 from kivy.graphics.texture import Texture
 import cv2
 
+
 class KivyCamera(Image):
     def __init__(self, capture, fps, **kwargs):
         super(KivyCamera, self).__init__(**kwargs)
         self.capture = capture
-        Clock.schedule_interval(self.update, 1.0/fps)
-    
+        Clock.schedule_interval(self.update, 1.0 / fps)
+
     def update(self, dt):
         ret, frame = self.capture.read()
         if ret:
@@ -21,7 +22,8 @@ class KivyCamera(Image):
             image_texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
             # display image from the texture
             self.texture = image_texture
-        
+
+
 class CamApp(App):
     def build(self):
         self.capture = cv2.VideoCapture(1)
@@ -29,8 +31,9 @@ class CamApp(App):
         return self.my_camera
 
     def on_stop(self):
-        #without this, app will not exit even if the window is closed
+        # without this, app will not exit even if the window is closed
         self.capture.release()
+
 
 if __name__ == "__main__":
     CamApp().run()
