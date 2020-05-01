29d5699c1c8117635befc7024d3045024ec79319
Noi Nguyen <noi.nguyen@lqdtu.edu.vn>
2020-05-01 09:12:50 +0700

    Update: face recognition

--- kivy_cv2/face_detection.py
+++ kivy_cv2/face_detection.py
@@ -5,6 +5,7 @@ import numpy as np
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 
+
 class SimpleKivy(App):
     def build(self):
 
@@ -13,33 +14,32 @@ class SimpleKivy(App):
         video_capture = cv2.VideoCapture(0)
         count = 0
         while (count < 40):
-            
+
             ret, frame = video_capture.read()
             print(np.shape(frame))
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            #gray = frame
+            # gray = frame
             faces = faceCascade.detectMultiScale(
                 gray,
                 scaleFactor=1.1,
                 minNeighbors=5,
                 minSize=(30, 30)
-                
+
             )
 
-            
             for (x, y, w, h) in faces:
-                cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-            
             cv2.imshow('Video', frame)
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
             count = count + 1
-        
+
         video_capture.release()
         cv2.destroyAllWindows()
-        return Label(text = "Face Detection Application")
-        
-if __name__=="__main__":
+        return Label(text="Face Detection Application")
+
+
+if __name__ == "__main__":
     SimpleKivy().run()
